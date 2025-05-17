6407140aed289c43a2cf7e9ad57cb11705dae0c0
Henry Schreiner <henryschreineriii@gmail.com>
2025-05-17 00:01:42 -0400

    fix: pull in fixes from scikit-build-core
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+from typing import Any
+
+import pytest
+
 import dynamic_metadata.loader
+import dynamic_metadata.plugins
 
 
 def test_template_basic() -> None:
@@ -61,12 +66,16 @@ def test_template_entry_points() -> None:
             },
             "entry-points": {
                 "provider": "dynamic_metadata.plugins.template",
-                "result": {"my_point": "my_app:script_{project[version]}"},
+                "result": {
+                    "my_group": {"my_point": "my_app:script_{project[version]}"}
+                },
             },
         },
     )
 
-    assert pyproject["entry-points"] == {"my_point": "my_app:script_1.2.3"}
+    assert pyproject["entry-points"] == {
+        "my_group": {"my_point": "my_app:script_1.2.3"}
+    }
 
 
 def test_regex() -> None:
@@ -87,3 +96,39 @@ def test_regex() -> None:
     )
 
     assert pyproject["requires-python"] == ">=dynamic-metadata"
+
+
+@pytest.mark.parametrize(
+    ("field", "input", "output"),
+    [
+        pytest.param("version", "{sub}", "42", id="str"),
+        pytest.param("classifiers", ["a", "{sub}"], ["a", "42"], id="list-str"),
+        pytest.param(
+            "scripts",
+            {"a": "{sub}", "{sub}": "b"},
+            {"a": "42", "42": "b"},
+            id="dict-str",
+        ),
+        pytest.param(
+            "authors", [{"name": "{sub}"}], [{"name": "42"}], id="list-dict-str"
+        ),
+        pytest.param(
+            "optional-dependencies",
+            {"dev": ["{sub}"]},
+            {"dev": ["42"]},
+            id="dict-list-str",
+        ),
+        pytest.param("readme", {"text": "{sub}"}, {"text": "42"}, id="readme"),
+        pytest.param(
+            "entry-points",
+            {"ep": {"{sub}": "{sub}"}},
+            {"ep": {"42": "42"}},
+            id="dict-dict-str",
+        ),
+    ],
+)
+def test_actions(field: str, input: Any, output: Any) -> None:
+    result = dynamic_metadata.plugins._process_dynamic_metadata(
+        field, lambda x: x.format(sub=42), input
+    )
+    assert output == result
